157909bb0d30ab3ad9ec173ef4b359403c766b71
Reuben Tochukwu Aniekwu <reuben.aniekwu@kellogg.northwestern.edu>
2024-01-25 03:28:22 +0000

    getting better at loops

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -7,5 +7,44 @@
 #   puts "tacos!"
 # end
 
+
+# The lnog way to do this would be:
+# puts "#{tacos[0]} tacos"
+# puts "#{tacos[1]} tacos"
+# puts "#{tacos[2]} tacos"
+# puts "#{tacos[3]} tacos"
+
 # Loop through tacos
-# tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+
+######shorter version of loop
+
+for taco in tacos
+    #do this in code
+    puts "#{taco} tacos"
+end
+
+
+######longer version of loop
+
+# #set the index to zero
+
+# index = 0
+
+# #start the loop
+# loop do
+
+#     if index == tacos.length   
+#         Break 
+#     end
+
+#     #put the taco at the index in memory
+#     taco = tacos[index]
+
+#     #write it to screen
+#     puts "#{taco} tacos" 
+
+#     # increment the index
+#     index = index + 1
+
+# end
